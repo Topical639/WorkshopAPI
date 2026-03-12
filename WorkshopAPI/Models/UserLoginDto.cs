a82123c4f0c62936bbf921a8f994b1e35d59cac0
chutipon.mai <chutipon.mai@tokiomarinelife.co.th>
2026-03-12 14:25:15 +0700

    lll

---
+++ WorkshopAPI/Models/UserLoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WorkshopAPI.Models
+{
+    public class UserLoginDto
+    {
+        [Required]
+        [JsonPropertyName("username")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [JsonPropertyName("password")]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ WorkshopAPI/Models/UserRegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WorkshopAPI.Models
+{
+    public class UserRegisterDto
+    {
+        [Required]
+        [JsonPropertyName("user_name")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [JsonPropertyName("email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        [JsonPropertyName("password")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
