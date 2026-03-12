097504bddc0abd09ef4abd4eb9e41f7aa0051cbd
chutipon.mai <chutipon.mai@tokiomarinelife.co.th>
2026-03-12 13:36:23 +0700

    uo

---
+++ WorkshopAPI/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WorkshopAPI.Models
+{
+   
+    public class User
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("user_name")]
+        public string Username { get; set; } = string.Empty;
+        [JsonPropertyName("email")]
+        public string Email { get; set; } = string.Empty;
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
