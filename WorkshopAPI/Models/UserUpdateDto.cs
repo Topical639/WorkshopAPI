d43419db507fc872d46d2cf945bc99d6965a18bb
chutipon.mai <chutipon.mai@tokiomarinelife.co.th>
2026-03-12 13:50:23 +0700

    ooo

---
+++ WorkshopAPI/Models/UserUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WorkshopAPI.Models
+{
+    public class UserUpdateDto
+    {
+        [JsonPropertyName("user_name")]
+        public string? Username { get; set; }
+
+        [JsonPropertyName("email")]
+        public string? Email { get; set; }
+
+    }
+}
