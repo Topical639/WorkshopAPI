7c04ca41ec2fc80b4d3fc35bfb0fedf0a37d9033
chutipon.mai <chutipon.mai@tokiomarinelife.co.th>
2026-03-12 13:43:23 +0700

    pp

--- WorkshopAPI/Data/UserStore.cs
+++ WorkshopAPI/Data/UserStore.cs
@@ -1,6 +1,25 @@
-﻿namespace WorkshopAPI.Data
+﻿using WorkshopAPI.Models;
+
+namespace WorkshopAPI.Data
 {
     public class UserStore
     {
+        public static List<User> Users = new()
+    {
+        new User
+        {
+            Id = 1,
+            Username = "john_doe",
+            Email = "john@example.com",
+            CreatedAt = DateTime.UtcNow
+        },
+        new User
+        {
+            Id = 2,
+            Username = "jane_smith",
+            Email = "jane@example.com",
+            CreatedAt = DateTime.UtcNow
+        }
+    };
     }
 }
